54bdf2d728f67c35fe7d2c3f51691700d761ac57
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-06-26 19:22:14 +0200

    ciao

---
+++ EpicodeBckendFS0223A/giorno_1/src/comm/epico/Main_Project.java
@@ -0,0 +1,10 @@
+package comm.epico;
+
+public class Main_Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EpicodeBckendFS0223A/m1/s1/g1/EpicodeBackendCourse1/src/it/epicode/be/Helloworld.java
@@ -0,0 +1,38 @@
+package it.epicode.be;
+
+
+
+public class Helloworld {
+
+public static void main (String[] args)
+		{
+		
+		System.out.println("This is my first Epicode Java Project!");
+		};
+
+
+
+	public static int moltiplica(int x , int y) {
+
+		int res = x * y;
+		return res;
+	};
+	
+	public static void stampaRes(int x , int y) {
+		int res = x * y;
+		System.out.println(res);
+	
+		
+	};
+	
+	public static String concatena(int str1 , String str2) {
+		String res = str1 + str2;
+		return res;
+	
+	}
+	
+	
+	
+	
+}
+
