f36ea45cab714a5d2dc40ea3d918471f8639436c
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-06-27 16:56:20 +0200

    finish

--- esercizio3/src/ecommerce/Cliente.java
+++ esercizio3/src/ecommerce/Cliente.java
@@ -1,21 +1,23 @@
 package ecommerce;
 
 public class Cliente {
-	private String codiceCliente;
-	private String nome;
-	private String cognome;
-	private String email;
-	private Cliente clienteAssociato;
-	private Carrello carrello;
-	private double totaleCostoArticoli;
-	
-	public Cliente(String codiceCliente, String nome, String cognome, String email) {
-	        this.codiceCliente = codiceCliente;
-	        this.nome = nome;
-	        this.cognome = cognome;
-	        this.email = email;
-	        this.carrello = new Carrello();
-	        this.totaleCostoArticoli = 0.0;
-	    }
-	
+    private String codiceCliente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private Cliente clienteAssociato;
+    private Carrello carrello;
+    private double totaleCostoArticoli;
+
+    public Cliente(String codiceCliente, String nome, String cognome, String email) {
+        this.codiceCliente = codiceCliente;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.carrello = new Carrello();
+        this.totaleCostoArticoli = 0.0;
+    }
+
+
 }
+
