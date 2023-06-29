3fe9da94be097c2d8382dd9bffa810351ac7f36d
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-06-29 18:02:12 +0200

    finish

--- giorno4/src/esercizioG4/Dipendente.java
+++ giorno4/src/esercizioG4/Dipendente.java
@@ -2,25 +2,26 @@ package esercizioG4;
 
 public class Dipendente {
 	
-	private static int matricola;
-	private static double stipendio;
-	private static double importoOrarioStraordinario;
-	private static Livello level;
-	private static Dipartimento department;
+	private static double stipendioBase = 1000;
+	private  int matricola;
+	private  double stipendio;
+	private  double importoOrarioStraordinario;
+	private  Livello level;
+	private  Dipartimento department;
 	
 	
 	
 	public Dipendente(int matricola , Dipartimento department) {
 		this.matricola = matricola;
 		this.department = department;
-		this.stipendio = 1000.0;
+		this.stipendio = stipendioBase;
 		this.importoOrarioStraordinario = 30;
 		this.level = Livello.OPERAIO;
 	}
 	
 	public Dipendente(int matricola ,double stipendio, double importoOrarioStraordinario, Livello level, Dipartimento department) {
 		this.matricola = matricola;
-		this.stipendio = stipendio;
+		this.stipendio = stipendioBase;
 		this.importoOrarioStraordinario = importoOrarioStraordinario;
 		this.level = level;
 		this.department = department;
@@ -51,7 +52,7 @@ public class Dipendente {
 	
 
 	
-	static public void stampaDatiDipendente() {
+	 public void stampaDatiDipendente() {
 		System.out.println("matricola " + matricola);
 		System.out.println("stipendio " + stipendio);
 		System.out.println("importo orario straordinario " + importoOrarioStraordinario);
@@ -59,28 +60,29 @@ public class Dipendente {
 		System.out.println("dipartimento " + department);
 	}
 	
-	static Livello promuovi() {
+	public Livello promuovi() {
 		
-		if(level == Livello.OPERAIO) {
-			stipendio *= 1.2;
-			return Livello.IMPIEGATO;
-				
+		if(this.level == Livello.OPERAIO) {
+			this.level = Livello.IMPIEGATO;
+				this.stipendio *= 1.2;
 		}else if(level == Livello.IMPIEGATO) {
-			stipendio *= 1.5;
-			return Livello.QUADRO;
-		}else if(Dipendente.level == Livello.QUADRO) {
-			stipendio *= 2;
-			return Livello.DIRIGENTE;
-		}
+			this.stipendio *= 1.5;
+			this.level = Livello.QUADRO;
+		}else if(this.level == Livello.DIRIGENTE) {
+			this.stipendio *= 2;
+			this.level = Livello.DIRIGENTE;
+		}else {
 		System.out.println("errore: impossibile promuovere il dipendente con livello DIRIGENTE.");
-		return level;
+		
+		}
+		return this.level;
 	}
 	
 	public static double calcolaPaga(Dipendente dipendente) {
-		return dipendente.getStipendio();
+		return dipendente.stipendio;
 	}
 	public static double calcolaPagaConStraordinario(Dipendente dipendente,int OreStraordinario) {
-		return dipendente.getStipendio() + (dipendente.getImportoOrarioStraordinario() * OreStraordinario);
+		return dipendente.stipendio + (dipendente.importoOrarioStraordinario * OreStraordinario);
 	}
 	
 	
--- giorno4/src/esercizioG4/GestioneDipendenti.java
+++ giorno4/src/esercizioG4/GestioneDipendenti.java
@@ -4,7 +4,7 @@ public class GestioneDipendenti {
 
 	public static void main(String[] args) {
 
-		Dipendente dip1 = new Dipendente(11 , 1400 , 15 ,Livello.OPERAIO,  Dipartimento.PRODUZIONE); 
+		Dipendente dip1 = new Dipendente(11 , Dipartimento.PRODUZIONE); 
 		Dipendente dip2 = new Dipendente(12 ,1400 , 25 , Livello.OPERAIO , Dipartimento.PRODUZIONE); 
 		Dipendente dip3 = new Dipendente(9 , 1800.0 , 30 , Livello.IMPIEGATO ,  Dipartimento.AMMINISTRAZIONE);
 		Dipendente dip4 = new Dipendente(3 , 2400.0 , 40 , Livello.DIRIGENTE ,  Dipartimento.VENDITE);
@@ -17,6 +17,18 @@ public class GestioneDipendenti {
 		dip3.stampaDatiDipendente();
 		dip4.stampaDatiDipendente();
 		
+		dip2.promuovi();
+		dip2.stampaDatiDipendente();
+		
+		System.out.println(Dipendente.calcolaPaga(dip1));
+		System.out.println(Dipendente.calcolaPaga(dip2));
+		System.out.println(Dipendente.calcolaPaga(dip3));
+		System.out.println(Dipendente.calcolaPaga(dip4));
+		
+		double sommaStipendiStraordinari = Dipendente.calcolaPaga(dip4) + 
+				Dipendente.calcolaPaga(dip3) + Dipendente.calcolaPaga(dip2) + Dipendente.calcolaPaga(dip1);
+		
+		System.out.println(sommaStipendiStraordinari);
 		
 		//dip1.setImportoOrarioStraordinario(5.2) + Dipendente.getStipendio();
 		//dip2.setImportoOrarioStraordinario(3.5);
